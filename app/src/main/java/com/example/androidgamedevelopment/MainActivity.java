4e30f4472696f58e9a3fa5a4e6cdd427d6343e44
k3v1n12 <ambalikevin@gmail.com>
2023-05-10 12:37:16 +0100

    Game Loop

--- app/src/main/java/com/example/androidgamedevelopment/MainActivity.java
+++ app/src/main/java/com/example/androidgamedevelopment/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.androidgamedevelopment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
+/**
+ * MainActivity is the entry point to our application
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        //set window to full screen (will hide the status bar)
+        getSupportActionBar().hide();
+        Window window = getWindow();
+        window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+        //Set Content View to game, so that object in the game class can be rendered
+        setContentView(new Game(this));
     }
 }
